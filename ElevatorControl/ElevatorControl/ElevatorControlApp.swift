ee021dce5c4deb4e769dbff3698e5cf82ccac503
Igor Terletskyi <ihorek12@gmail.com>
2025-09-03 14:11:53 +0300

    init project

---
+++ ElevatorControl/ElevatorControl/ElevatorControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElevatorControlApp.swift
+//  ElevatorControl
+//
+//  Created by Igor Terletskyi on 03.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct ElevatorControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
